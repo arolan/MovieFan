7e5caf5d4fd244a10967b33baa6e96c490b59eb4
Rolan Marat <arolan@gmail.com>
2022-09-04 20:17:40 -0700

    Initial Commit

---
+++ MovieFan/MovieFanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFanApp.swift
+//  MovieFan
+//
+//  Created by Rolan on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieFanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
